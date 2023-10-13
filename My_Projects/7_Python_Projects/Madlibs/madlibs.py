fbdb0333a46043a11c8917bb34c7c919144ba1bb
Gabriel Bernabé García <gabrielfutsal3@gmail.com>
2023-10-14 00:55:39 +0200

    addition

---
+++ My_Projects/7_Python_Projects/Madlibs/madlibs.py
@@ -0,0 +1,14 @@
+#String concatenation
+
+# youtuber = "Fernando Alonso"
+#
+# print(f"Subscribe to {youtuber}")
+
+Name = input("Your name: ")
+Last_Name = input("Your lastname: ")
+Age = input("Your age: ")
+Location = input("Your location: ")
+
+Result = f"Hi I'm {Name} {Last_Name}. I'm {Age} and I live in {Location}"
+
+print(Result)
